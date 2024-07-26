849808f287872d4f1e4fad945623f972b85dec4e
Jaeger_yan <appie40@outlook.com>
2024-07-26 21:36:23 +0800

    Initial commit

---
+++ src/main/java/com/uvic/csc370assignment5/Csc370Assignment5Application.java
@@ -0,0 +1,13 @@
+package com.uvic.csc370assignment5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc370Assignment5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csc370Assignment5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uvic/csc370assignment5/Csc370Assignment5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvic.csc370assignment5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc370Assignment5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
